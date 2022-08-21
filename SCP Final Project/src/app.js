d863b51a31d1eed21529b15f22da86a941230c84
AlexWalesDragoness <110855001+AlexWalesDragoness@users.noreply.github.com>
2022-08-21 20:48:52 +0100

    worked on JS current temp

---
+++ SCP Final Project/src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "cc19da36ddd36220c1e743c979d7284e";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric";
+axios.get(apiUrl).then(displayTemperature);
