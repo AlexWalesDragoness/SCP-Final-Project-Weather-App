02d2e96ae428703fae506e72f8827b57e859aa24
AlexWalesDragoness <110855001+AlexWalesDragoness@users.noreply.github.com>
2022-08-21 21:59:12 +0100

    updated relevant weather icon

--- SCP Final Project/src/app.js
+++ SCP Final Project/src/app.js
@@ -3,46 +3,56 @@ function formatDate(timestamp) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}:${minutes}`;
   }
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
+  function displayTemperature(response) {
+    let temperatureElement = document.querySelector("temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
-  return `${day} ${hours}:${minutes}`;
+    let cityElement = document.querySelector("city");
+    cityElement.innerHTML = response.data.name;
+
+    let descriptionElement = document.querySelector("description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+    let humidityElement = document.querySelector("humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+
+    let windElement = document.querySelector("wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+    let dateElement = document.querySelector("date");
+    dateElement.innerHTML = formatDate(response.date.dt * 1000);
+
+    let iconElement = document.querySelector("icon");
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+
+    iconElement.setAttribute("Alt", `${response.data.weather[0]).description}`);
+  }
+
+  let apiKey = "cc19da36ddd36220c1e743c979d7284e";
+  let city = London;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
-
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
-  let cityElement = document.querySelector("city");
-  cityElement.innerHTML = response.data.name;
-
-  let descriptionElement = document.querySelector("description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
-
-  let humidityElement = document.querySelector("humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
-
-  let windElement = document.querySelector("wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-
-  let dateElement = document.querySelector("date");
-  dateElement.innerHTML = formatDate(response.date.dt * 1000);
-}
-
-let apiKey = "cc19da36ddd36220c1e743c979d7284e";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric";
-axios.get(apiUrl).then(displayTemperature);
